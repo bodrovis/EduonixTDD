875308f40308114a5e5f4388952032478cbb592c
bodrovis <bodrovis@protonmail.com>
2017-08-08 15:44:12 +0300

    7.1

--- test/system/subscriptions_test.rb
+++ test/system/subscriptions_test.rb
@@ -6,7 +6,7 @@ class SubscriptionsTest < ApplicationSystemTestCase
     my_cookies = ActionDispatch::Request.new(Rails.application.env_config).cookie_jar
     my_cookies.signed[:user_id] = @user.id
   end
-  test "visiting the index" do
+  test "user should be able to subscribe to categories" do
     visit root_url
     assert page.has_css?('h1', text: 'Subscriptions')
     within '#subscriptions' do
@@ -14,7 +14,17 @@ class SubscriptionsTest < ApplicationSystemTestCase
     end
 
     within '#new-subscription' do
-      
+      select('java', from: 'subscription[category]')
+      click_button 'Subscribe!'
     end
+
+    within '#subscriptions' do
+      assert page.has_content?('java')
+    end
+    within '#new-subscription' do
+      assert page.has_select?('subscription[category]', options: %w(php python))
+      assert_selector '[name="subscription[category]"] option', count: 2
+    end
+    take_screenshot
   end
 end
